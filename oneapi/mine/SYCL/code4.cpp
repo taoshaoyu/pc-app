40efc74f6ed2dd27495803662664a23d6596b80e
taosy <shaoyu.tao@intel.com>
2024-03-06 02:46:33 +0800

    add code4

---
+++ oneapi/mine/SYCL/code4.cpp
@@ -0,0 +1,26 @@
+// Copyright (C) 2023 Intel Corporation
+
+// SPDX-License-Identifier: MIT
+
+#include <iostream>
+#include <sycl/sycl.hpp>
+using namespace sycl;
+
+int main() {
+  // Create queue bound to an available GPU device
+  queue q{gpu_selector_v};
+
+  std::cout << "Selected device: "
+            << q.get_device().get_info<info::device::name>()
+            << "\n";
+  std::cout
+      << " -> Device vendor: "
+      << q.get_device().get_info<info::device::vendor>()
+      << "\n";
+
+  return 0;
+}
+
+// Example Output:
+// Selected device: AMD Radeon RX 5700 XT
+// -> Device vendor: AMD Corporation
