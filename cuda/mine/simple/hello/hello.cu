699e7487fa10996dc8483029f5518d0e730be683
taosy <shaoyu.tao@intel.com>
2024-03-14 00:06:21 +0800

    fixbug

--- cuda/mine/simple/hello/hello.cu
+++ cuda/mine/simple/hello/hello.cu
@@ -1,12 +1,26 @@
 #include <stdio.h>
 
 __global__ void helloFromGPU(void){
-    printf("hello world from GPU\n");
+    printf("hello world from GPU, blockIdx.x=%d threadIdx.x=%d\n", blockIdx.x, threadIdx.x);
 }
+
+__global__ void dimTest(void){
+    int block_idx = blockIdx.x + gridDim.x * gridDim.y * blockIdx.y; 
+    int thread_idx = threadIdx.x + blockDim.x * blockDim.y * threadIdx.y;
+//    printf("block_idx, thread_idx (%d, %d)\n", block_idx, thread_idx);
+    printf("blockIdx, threadIdx (%d, %d, %d, %d) gridm(%d, %d, %d), blockDim(%d, %d, %d)\n", blockIdx.x, blockIdx.y, threadIdx.x, threadIdx.y, gridDim.x, gridDim.y, gridDim.z, blockDim.x, blockDim.y, blockDim.z);
+}
+
 
 int main(void){
     printf("hello world from CPU\n");
-    helloFromGPU <<<1,3>>>();
+    helloFromGPU <<<2,3>>>();
+    dim3 grid(3,2);
+    dim3 blockdim(2,3);
+    dimTest<<<grid,blockdim>>>();
+    
+    printf("%d\n",grid);
+
     cudaDeviceReset();
     return 0;
 }
